98178cdd2ffda431d0e8bd71314634003eec03c6
Chad Tolkien <chad@tolkien.id.au>
2013-01-10 13:02:17 +1100

    still working on rebuilding teh api

--- Badmin.Tests/Class1.cs
+++ Badmin.Tests/Class1.cs
@@ -1,4 +1,5 @@
-﻿using AutoMapper;
+﻿using System.Web.Mvc;
+using AutoMapper;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -25,31 +26,166 @@ namespace Badmin.Tests
 
     }
 
-    public class Monkey
+
+    public class Tests
     {
+    	
+        [Fact]
+        public void BadminCanRegisterADataSource()
+        {
+            var badmin = new Badmin<DatabaseDummyContext>();
+            Assert.IsType<DatabaseDummyContext>(badmin.CreateDataContextType());
+
+        }
 
         [Fact]
-        public void BadminCanRegisterIndividualIDbSets()
+        public void BadminCanRegisterTypesToBeModified()
         {
-
+        	
             var badmin = new Badmin<DatabaseDummyContext>();
 
             badmin.Register<Dummy>(x => x.Dummies);
 
+            Assert.Equal(1, badmin.DataConfigurations.Count);
+            
+
+            var first = badmin.DataConfigurations.First();
+            Assert.IsType(typeof(DbSet<Dummy>), first.Data);
+        }
+
+        [Fact]
+        public void BadminCanRegisterMultipleTypesOfTheSameType()
+        {
+            var badmin = new Badmin<DatabaseDummyContext>();
+
+            badmin.Register<Dummy>(x => x.Dummies);
+            badmin.Register<Dummy>(x => x.Dummies);
+
+            Assert.Equal(2, badmin.DataConfigurations.Count);
+        }
+
+        [Fact]
+        public void BadminCanSetNameOfRegisteredDataCollection()
+        {
+            var badmin = new Badmin<DatabaseDummyContext>();
+
+            badmin.Register<Dummy>(x => x.Dummies)
+                .Name("TooCool");
+
+            Assert.Equal("TooCool", badmin.DataConfigurations.First().Name);
 
         }
+
     }
 
-    public class Badmin<T> where T : class
+    public static class BadminExtensions
+    {
+    	public static BadminConfiguration Name(this BadminConfiguration config, string name)
+    	{
+            config.DataConfiguration.Name = name;
+            return config;
+    	}
+
+
+    }
+
+    public class BadminConfiguration
     {
         
-        //ICollection<IDbSet<TResult>> things;
+        public BadminConfiguration(DataConfiguration dataConfiguration)
+        {
+            this.DataConfiguration = dataConfiguration;
+        }
 
-        public void Register<TResult>(Func<T, IDbSet<TResult>> wootah) where TResult: class
+
+        public DataConfiguration DataConfiguration { get; set; }
+        
+	
+    }
+    
+    public class Badmin<T> where T: class 
+    {
+
+        public Badmin()
+        {
+            DataConfigurations = new List<DataConfiguration>();
+        }
+
+
+        public ICollection<DataConfiguration> DataConfigurations { get; set; }
+
+        
+
+        public BadminConfiguration Register<TResult>(Func<T, IQueryable<TResult>> data) where TResult: class
         {
             
+            var dataContext = this.CreateDataContextType();
+
+            var invokedData = data.Invoke(dataContext);
+
+            var dataConfiguration = new DataConfiguration
+            {
+                Data = invokedData
+            };
+
+            DataConfigurations.Add(dataConfiguration);
+
+            return new BadminConfiguration(dataConfiguration);
+           
         }
+
+        public T CreateDataContextType()
+        {
+            var dataContext = typeof(T).GetConstructor(System.Type.EmptyTypes).Invoke(null);
+
+            return dataContext as T;
+
+        }
+
+
+
+
+    }
+    public class DataConfiguration
+    {
+        public string Name { get; set; }
+        public IQueryable Data { get; set; }
+        public Type Type { get; set; }
+    }
+
+    
+    //public class Badmin<T>
+    //{
+    //    private readonly DatabaseDummyContext dataContext;
+
+    //    public Badmin(DatabaseDummyContext dataContext)
+    //    {
+    //        this.dataContext = dataContext;
+    //    }
+
+    //    public BadminConfiguration Register<TResult>(Func<T, IQueryable<TResult>> data) where TResult : class
+    //    {
+            
+    //        var invokedDelegate = data.Invoke(this.dataContext);
+
+    //        return new BadminConfiguration(invokedDelegate));
+    //    }
+
+    //    public IEnumerable<BadminConfiguration> Configurations { get; private set; }
+       
+    //}
+
+    public class BadminDataSource<T> //T is the underlying data type.
+    {
     	
+        public readonly string Name;
+
+        public BadminDataSource(IQueryable dataSource, string name)
+        {
+            this.Name = name;
+        }
+
+
     }
 
 
